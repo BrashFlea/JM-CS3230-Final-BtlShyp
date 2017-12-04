8d02d9282a26899e0c53e07358d58fb3097740ca
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-12-03 17:25:45 -0700

    Added opponent's game board

--- src/BtlShypUserInterface/userInterface.java
+++ src/BtlShypUserInterface/userInterface.java
@@ -13,9 +13,38 @@ public class userInterface extends View {
 
   private static final long serialVersionUID = 1L;
 
-  private static JPanel shipGridArea() {
+  private static JPanel playerShipGridArea() {
     JPanel shipGrid = new JPanel(new MigLayout("debug, fill"));
 
+    String[] colLabels = {"A","B","C","D","E"};
+    String buttonLabel = "";
+
+    for (int i = 0; i < 5; i++) {
+      for(int j = 0; j < 5; j++) {
+        buttonLabel = colLabels[j] + Integer.toString(i+1);
+        //TODO Change to AttackButtons and pass in coordinates
+        JButton button = new JButton(buttonLabel);
+        
+        button.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            //button.setText("P");          
+          }
+        });
+          
+        if (j == 4) {
+          shipGrid.add(button, "wrap, grow"); 
+        }
+        else {
+          shipGrid.add(button, "grow");
+        }
+      }
+    }
+    return shipGrid;
+  }
+  
+  private static JPanel opponentShipGridArea() {
+    JPanel shipGrid = new JPanel(new MigLayout("debug, fill"));
+    
     String[] colLabels = {"A","B","C","D","E"};
     String buttonLabel = "";
 
@@ -35,25 +64,25 @@ public class userInterface extends View {
   }
 
   private static JPanel checkForPlayagain() {
-    JPanel playAgain = new JPanel(new MigLayout());
-    playAgain.add(new JLabel("Would you like to play again?"), "wrap");
-    playAgain.add(new JButton("Yes"));
+    JPanel playAgain = new JPanel(new MigLayout("debug, fill"));
+    playAgain.add(new JLabel("Would you like to play again?"), "wrap, center");
+    JButton yesButton = new JButton("Yes");
     JButton noButton = new JButton("No");
-
+  
+    yesButton.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent evt) {
+        //playAgain();
+      }
+    });
+    
     noButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
         System.exit(0);
       }
     });
-
-    playAgain.add(noButton);
-
-    /*
-    yes.addActionListener(new ActionListener() {
-    public void actionPerformed(ActionEvent evt) {
-        exitGame();
-      }
-    });*/
+    
+    playAgain.add(yesButton, "grow, center");
+    playAgain.add(noButton, "grow, center");
 
     return playAgain;
 
@@ -81,20 +110,27 @@ public class userInterface extends View {
           ex.printStackTrace();
         }
 
-        /*
-        JFrame playAgainFrame = new JFrame("Example 01");
+        
+        JFrame playAgainFrame = new JFrame("Jonathan Mirabile - BtlShyp");
         playAgainFrame.getContentPane().add(checkForPlayagain());
         playAgainFrame.pack();
         playAgainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         playAgainFrame.setVisible(true);
-        */
+        
          
         JFrame shipGridFrame = new JFrame("Jonathan Mirabile - BtlShyp");
-        shipGridFrame.getContentPane().add(shipGridArea());
-        shipGridFrame.setPreferredSize(new Dimension(750,750));
+        JPanel gridBoard = new JPanel(new MigLayout("debug"));
+        //shipGridFrame.setPreferredSize(new Dimension(750,750));
+        
+        gridBoard.add(playerShipGridArea());
+        gridBoard.add(opponentShipGridArea());
+        
+        
+        shipGridFrame.add(gridBoard);
         shipGridFrame.pack();
         shipGridFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         shipGridFrame.setVisible(true);
+        
 
       }
     });
